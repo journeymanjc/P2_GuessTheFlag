8c75cd3574d6d745c7e07815e56a37340dff9de5
Jae Cho <jjaecho@gmail.com>
2022-02-02 14:20:46 -0500

    Day 20 Completed

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var showingAlert: Bool = false
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		 ZStack{
+			 VStack{
+				 Color.red
+				 Color.blue
+				 
+				 VStack{
+					 Button("Button 1"){
+						 showingAlert = true
+						 
+					 }
+					 .buttonStyle(.bordered)
+					 Button("Button 2", role: .destructive){}
+					 .buttonStyle(.bordered)
+					 Button("Button 3", role: .destructive){}
+					 .buttonStyle(.borderedProminent)
+					 
+					 Button{
+						 print("Button was tapped")
+					 } label: {
+						 Text("Tap me!")
+							 .padding()
+							 .foregroundColor(.white)
+							 .background(.red)
+					 }
+					 .alert("Important message", isPresented: $showingAlert){
+						 Button("Delete", role: .destructive) {}
+						 Button("Cancel", role:.cancel) {}
+
+					 } message: {
+						 Text("Hows it going")
+					 }
+				 }
+			 }
+			 
+			 Text("Your Content")
+				 .foregroundStyle(.secondary)
+				 .padding(20)
+				 .background(.ultraThinMaterial)
+			 
+			 
+		 }
+		 
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+			 .previewDevice("iPhone 13 Mini")
     }
 }
